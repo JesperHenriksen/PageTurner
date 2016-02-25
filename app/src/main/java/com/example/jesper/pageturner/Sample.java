5160655250cbe5dfa789075567cd01f48fda0927
Natasjah <nhelle14@student.aau.dk>
2016-02-25 13:02:11 +0100

    Sample.java: Added functions

--- app/src/main/java/com/example/jesper/pageturner/Sample.java
+++ app/src/main/java/com/example/jesper/pageturner/Sample.java
@@ -1,11 +1,27 @@
 package com.example.jesper.pageturner;
 
-/**
- * Created by Jesper on 25-02-2016.
- */
+//Puts a byte in an Array list.
+
+import java.util.ArrayList;
+
 public class Sample {
 
-    public Analog;
-    public Analog;
+    public double digitalSample;
+    ArrayList<Byte> byteArray = new ArrayList<Byte>();
 
+    Sample(Byte b) { //Sample Constructor. Creates one sample with one byte
+        byteArray.add(b);
+    }
+
+    public void addByteToList(Byte b){ //Add more bytes to the List
+        byteArray.add(b);
+    }
+
+    public double getDigital() {
+        return digitalSample;
+    }
+
+    public void setDigital(double digital) {
+        this.digitalSample = digital;
+    }
 }
