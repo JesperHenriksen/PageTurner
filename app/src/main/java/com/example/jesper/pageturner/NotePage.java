d2b522958690f607b44755fe2c56800ec03a215e
Natasjah <nhelle14@student.aau.dk>
2016-03-21 09:02:54 +0100

    Created spinner

--- app/src/main/java/com/example/jesper/pageturner/NotePage.java
+++ app/src/main/java/com/example/jesper/pageturner/NotePage.java
@@ -1,5 +1,6 @@
 package com.example.jesper.pageturner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,14 +16,12 @@ public class NotePage extends AppCompatActivity {
         setContentView(R.layout.activity_note_page);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    public void backChooseSong(View view)
+    {
+        Intent intent = new Intent(NotePage.this, ChooseSong.class);
+        startActivity(intent);
     }
 
 }
