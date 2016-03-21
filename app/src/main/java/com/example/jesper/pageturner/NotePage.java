7207ac68f33aa4529b0842e9b9b430ef0bcd1826
Natasjah <nhelle14@student.aau.dk>
2016-03-21 14:57:21 +0100

    Overflow menu

--- app/src/main/java/com/example/jesper/pageturner/NotePage.java
+++ app/src/main/java/com/example/jesper/pageturner/NotePage.java
@@ -3,57 +3,41 @@ package com.example.jesper.pageturner;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 import android.widget.SpinnerAdapter;
 
-public class NotePage extends Activity {
+public class NotePage extends AppCompatActivity {
 
-    String[] options = {" ", "Reset Song", "Show  Sheet Music", "Show Progress Bar"};
-    int arr_images[] = {R.drawable.dropdownmenu, R.drawable.resetmenu, R.drawable.notemenu, R.drawable.progressmenu};
 
-    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_page);
 
-        spinner = (Spinner)findViewById(R.id.dropDownMenu);
-        spinner.setAdapter(new SpinnerAdapter(this, R.layout.spinner_layout, options));
+        Toolbar myToolBar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolBar);
+
     }
 
-    public class SpinnerAdapter extends ArrayAdapter<String>{
-        public SpinnerAdapter(Context context, int textViewResourceId, String[] objects){
-            super(context, textViewResourceId, objects);
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.tool_menu_options, menu);
 
-        public View getDropDownView(int position, View convertView, ViewGroup parent){
-            return getSpinnerView(position, convertView, parent);
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent){
-            return getSpinnerView(position, convertView, parent);
-        }
-
-        public View getSpinnerView(int position, View convertView, ViewGroup parent){
-
-            LayoutInflater inflater=getLayoutInflater();
-            View row=inflater.inflate(R.layout.spinner_layout, parent, false);
-            TextView label= (TextView)row.findViewById(R.id.menuItems);
-            label.setText(options[position]);
-
-            ImageView icon=(ImageView)row.findViewById(R.id.spinnerIcons);
-            icon.setImageResource(arr_images[position]);
-            return row;
-        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void backChooseSong(View view)
